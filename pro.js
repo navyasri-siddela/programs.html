a16214e11ee444e3c9dedb4b31bf01d10b721e1a
navyasri-siddela <siddelakeerthi4@gmail.com>
2025-02-11 00:12:58 -0800

    Add files via upload

---
+++ pro.js
@@ -0,0 +1,12 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="vieport"content="width=device-width,initial-scale 
+        <title>java script example</title>
+</head>
+<body>
+  <h1 id="heading">hllo,java script!</h1>
+  <button id="btn">click me</button>
+  <script>
+    //DOM manipulaton
+    const heading =document.getelementary("heading");
